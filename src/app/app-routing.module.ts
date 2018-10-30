7ed3c98c1b3ce8b3ae43ca04f8594dc9f3ab1582
Ramesh Kathiravan <ramesh.kathiravan@consultant.volvo.com>
2018-10-30 16:05:29 +0530

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {DepartmentListComponent} from "./department/department-list/department-list.component";
-import {AboutUsComponent} from "./about-us/about-us.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'employee', loadChildren: './employee/employee.module#EmployeeModule'},
-  {path: 'departments', component: DepartmentListComponent},
-  {path: 'aboutus', component: AboutUsComponent},
-  {path: '**', component: PageNotFoundComponent}
+  {path: 'employee', loadChildren: './employee/employee.module#EmployeeModule'}
 ];
 
 
@@ -21,5 +15,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const RoutingComponents = [HomeComponent, NavbarComponent, DepartmentListComponent, AboutUsComponent, PageNotFoundComponent];
+export const RoutingComponents = [HomeComponent, NavbarComponent];
 
